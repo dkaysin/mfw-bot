04aeaeb8071f075b05e69dd6e175d31a3637320a
kaysinds <kaysin.dmitry@gmail.com>
2017-04-16 22:29:25 +0300

    Add Heroku Connectivity

--- prepareForDeploy.sh
+++ prepareForDeploy.sh
@@ -1,1 +1,4 @@
 godep save ./...
+git add .
+git commit -m "Add Heroku Connectivity"
+git push origin master
--- server.go
+++ server.go
@@ -16,7 +16,47 @@ func API_KEY() string {
 	return os.Getenv("MFWBOT_API_KEY")
 }
 
-func connect() <-chan tgbotapi.Update {
+func connectWebHook() <-chan tgbotapi.Update {
+
+	log.Printf("Using API key: %v", API_KEY())
+
+	var updatesC <-chan tgbotapi.Update
+
+	for {
+		b, err := tgbotapi.NewBotAPI(API_KEY())
+		if err != nil {
+			log.Println("[server] No connection. Reconnecting after timeout...")
+			time.Sleep(5 * time.Second)
+		} else {
+			Bot = b
+			break
+		}
+	}
+
+	log.Printf("[server] Authorized on account %s", Bot.Self.UserName)
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://mfw-bot.herokuapp.com:/"+os.Getenv("PORT")+bot.Token, "cert.pem"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		u, err := Bot.ListenForWebhook("/" + Bot.Token)
+		if err != nil {
+			log.Println("[server] Unable to set updates handle. Reconnecting after timeout...")
+			time.Sleep(5 * time.Second)
+		} else {
+			updatesC = u
+			break
+		}
+	}
+
+	go http.ListenAndServeTLS("0.0.0.0:"+os.Getenv("PORT"), "cert.pem", "key.pem", nil)
+
+	return updatesC
+}
+
+func connectLongPolling() <-chan tgbotapi.Update {
 
 	log.Printf("Using API key: %v", API_KEY())
 
@@ -151,7 +191,7 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	GetData(&Dict)
 
-	updates := connect()
+	updates := connectWebHook()
 
 	for update := range updates {
 		if msg := update.Message; msg != nil {
